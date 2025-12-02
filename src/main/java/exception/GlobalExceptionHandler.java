6ff1f84f3eec2748b8b3546a31a2cab34a3baccf
poonamap3006-a11y <poonamap3006@gmail.com>
2025-12-02 17:40:06 +0530

    Update GlobalExceptionHandler.java

--- src/main/java/exception/GlobalExceptionHandler.java
+++ src/main/java/exception/GlobalExceptionHandler.java
@@ -12,20 +12,17 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(ResourceAccessException.class)
 	public ResponseEntity<String> handleServiceUnavailable(ResourceAccessException ex) {
-		// Log the detailed exception here
 		return new ResponseEntity<>("External API (MealDB) is currently unavailable or unreachable.",
 				HttpStatus.SERVICE_UNAVAILABLE);
 	}
 
 	@ExceptionHandler(HttpClientErrorException.class)
 	public ResponseEntity<String> handleHttpClientError(HttpClientErrorException ex) {
-		// Can extract more details if needed
 		return new ResponseEntity<>("Error retrieving data from MealDB: " + ex.getStatusText(), ex.getStatusCode());
 	}
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<String> handleGenericException(Exception ex) {
-		// Log the detailed exception here
 		return new ResponseEntity<>("An unexpected error occurred: " + ex.getMessage(),
 				HttpStatus.INTERNAL_SERVER_ERROR);
 	}
